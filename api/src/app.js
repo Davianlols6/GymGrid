835a305021171139dc18d957adcd47c9412a1f45
Davianlols6 <davianenghongrui@gmail.com>
2024-02-27 17:26:46 +0800

    Implemented setting cookie exmaple

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mainRoutes = require('./routes/mainRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/", mainRoutes);
 
--- api/src/routes/mainRoutes.js
+++ api/src/routes/mainRoutes.js
@@ -1,4 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+// Define a route to set a cookie
+router.get('/set-cookie', (req, res) => {
+    // Set a cookie named 'user' with the value 'john_doe'
+    res.cookie('user', 'john_doe', { httpOnly: true, secure: true, sameSite: 'Strict' });
+
+    // Send a response
+    res.send('Cookie has been set');
+});
+
+// Define a route to read the cookie
+router.get('/get-cookie', (req, res) => {
+    // Retrieve the value of the 'user' cookie
+    const userCookie = req.cookies.user;
+
+    // Send the value as a response
+    res.send(`User: ${userCookie}`);
+});
+
+// Define a route to remove the cookie
+router.get('/remove-cookie', (req, res) => {
+    // Clear the 'user' cookie
+    res.clearCookie('user');
+  
+    // Send a response
+    res.send('Cookie has been removed');
+  });
+
 module.exports = router;
